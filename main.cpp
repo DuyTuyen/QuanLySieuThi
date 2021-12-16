3ac0019d53e21ffab47b34e33d66ad68dea0a555
DuyTuyen <93909019+DuyTuyen@users.noreply.github.com>
2021-12-16 13:41:18 +0700

    Thêm chức năng xuất thống kê nhân viên
    Xuất thống kê nhân viên theo số lượng nhân viên trong file text

--- main.cpp
+++ main.cpp
@@ -58,3 +58,70 @@ void nhap(danhSachST& list) {
 		p = p->next;
 	}
 }
+void ghiNode(fstream& f, danhSachST list)
+{
+	string duoi[10];		// Khai báo biến kiểu string để lưu giá trị
+	string giua[10];
+	string tren[10];
+	int duoiIndex = 0, giuaIndex = 0, trenIndex = 0;
+
+	SieuThi* p = list.danhSachST;
+	if (p != NULL) {
+		while (p != NULL)
+		{
+			if (p->soLuongNhanVien < 100) {
+				duoi[duoiIndex++] = p->tenST;
+			}
+			else if (p->soLuongNhanVien >= 100 && p->soLuongNhanVien <= 200) {
+				giua[giuaIndex++] = p->tenST;
+			}
+			else {
+				tren[trenIndex++] = p->tenST;
+			}
+			p = p->next;
+		}
+	}
+
+	f << "So luong sieu thi duoi 100 la : " << duoiIndex << endl;
+	for (int i = 0; i < duoiIndex; ++i)
+		f << "\tsieu thi: " << duoi[i] << endl;
+	f << "So luong sieu thi tu 100 den 200 la : " << giuaIndex << endl;
+	for (int i = 0; i < giuaIndex; ++i)
+		f << "\tsieu thi: " << giua[i] << endl;
+	f << "So luong sieu thi tren 200 la : " << trenIndex << endl;
+	for (int i = 0; i < trenIndex; ++i)
+		f << "\tsieu thi: " << tren[i] << endl;
+}
+
+void ghifile(string filename, danhSachST list)
+{
+	fstream f;
+
+	f.open(filename, ios::out);			// Mở file để ghi
+	if (f.is_open()) {
+		cout << "Da ghi duoc : \n";
+		ghiNode(f, list);
+	}
+	else
+		cout << "Khong mo duoc file";
+	f.close();
+}
+
+void docfile(string filename)
+{
+	fstream f;		// Tạo đường dẫn đến file.txt
+	f.open(filename, ios::in);// Mở file  để đọc
+	string line;		// Để hứng từng dòng
+	cin.ignore(1);	// Xóa kí tự enter
+	int index = 0;
+	if (f.is_open()) {// Mở file ra
+		while (!f.eof())
+		{
+			getline(f, line);
+			cout << line << endl;
+		}
+		f.close();
+	}
+	else		// Không mở được thì chạy xuống đây
+		cout << "Khong mo duoc file";
+}
