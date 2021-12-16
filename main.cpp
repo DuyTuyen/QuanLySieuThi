a27d5fb3056e7e4d56aa13a28bce7d4071ca0f80
minhanzenc <93908719+minhanzenc@users.noreply.github.com>
2021-12-16 13:32:54 +0700

    Đã thêm chức năng in ra siêu thị có số lượng nhân viên max ,min
    Tạo ra 2 biến max min và đem số lượng nhân viên của siêu thị đem so sánh với biến max nếu max < soluongnv thì cập nhật lại max ,min tương tự với max nhưng là soluongnv< min

--- main.cpp
+++ main.cpp
@@ -20,41 +20,35 @@ void init(danhSachST& list) {		// Hàm khởi tạo init
 	list.danhSachST = NULL;
 	list.soLuong = 0;
 }
-SieuThi* nhapThongTinSieuThi() {		// Hàm nhập thông tin siêu thị
-	SieuThi* tam = new SieuThi();
-	cout << "Nhap ma sieu thi: ";
-	cin.getline(tam->maST, 50);
-	cout << "Nhap ten sieu thi: ";
-	cin.getline(tam->tenST, 50);
-	cout << "Nhap nam thanh lap: ";
-	cin >> tam->namThanhLap;
-	cout << "Nhap doanh so co ban: ";
-	cin >> tam->doanhSoCoBan;
-	cout << "Nhap so luong nhan vien: ";
-	cin >> tam->soLuongNhanVien;
-	cin.ignore();
-	tam->next = NULL;
-	return tam;
-}
-void nhap(danhSachST& list) {		
-	SieuThi* p = list.danhSachST;		// Gán con trỏ p
-	int k = 0;							// Biến k chứa số lượng siêu thị nhập vào
-	cout << "Nhap so luong sieu thi trong danh sach:";
-	cin >> k;
-	cin.ignore();
-
+void inMaxMinNhanVien(danhSachST list) {
+	SieuThi* p = list.danhSachST;
+	int max1 = 0, min1 = 9999;
+	SieuThi* k = list.danhSachST;
+	SieuThi* l = list.danhSachST;
 	if (p != NULL) {
-		while (p->next != NULL) {	// Duyệt danh sách
+		while (p != NULL) {
+			if (p->soLuongNhanVien > max1) {			// Thuat toan tim so luong nhan vien max
+				max1 = p->soLuongNhanVien;
+				k = p;
+			}
+			if (p->soLuongNhanVien < min1) {			// Thuat toan tim so luong nhan vien min
+				min1 = p->soLuongNhanVien;
+				l = p;
+			}
 			p = p->next;
-		}
-	}
-	else {
-		list.danhSachST = nhapThongTinSieuThi();  // Nếu rỗng thì cho nhập thông tin
-		p = list.danhSachST;
-	}
+		};
+		cout << "Danh sach cac sieu thi co so luong nhan vien lon nhat:";
+		cout << "Ma sieu thi: " << k->maST << endl;
+		cout << "Ten sieu thi: " << k->tenST << endl;
+		cout << "Nam thanh lap: " << k->namThanhLap << endl;
+		cout << "Doanh so co ban: " << k->doanhSoCoBan << endl;
+		cout << "So luong nhan vien: " << k->soLuongNhanVien << endl;
 
-	for (int i = 0; i < k - 1; i++) {		// Chạy for để nhập đủ số lượng k đã yêu cầu
-		p->next = nhapThongTinSieuThi();
-		p = p->next;
+		cout << "Danh sach cac sieu thi co so luong nhan vien nho nhat:";
+		cout << "Ma sieu thi: " << l->maST << endl;
+		cout << "Ten sieu thi: " << l->tenST << endl;
+		cout << "Nam thanh lap: " << l->namThanhLap << endl;
+		cout << "Doanh so co ban: " << l->doanhSoCoBan << endl;
+		cout << "So luong nhan vien: " << l->soLuongNhanVien << endl;
 	}
 }
